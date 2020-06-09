ca46c25526c996f5228e482535b197c0f6512a4e
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-09 23:03:19 +0530

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -105,17 +105,27 @@ class HomePage extends StatelessWidget {
                     top: 10.0,
                     right: 20.0,
                   ),
-                  child: Container(
-                    height: 60.0,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Color(0xFF18D191),
-                        borderRadius: BorderRadius.circular(9.0)),
-                    child: Text(
-                      'Sign in with Email',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 60.0,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF18D191),
+                          borderRadius: BorderRadius.circular(9.0)),
+                      child: Text(
+                        'Sign in with email',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
@@ -174,6 +184,183 @@ class HomePage extends StatelessWidget {
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Color(0xFF18D191)),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFF18D191),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Icon(
+                  Icons.local_offer,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 60.0,
+                width: 60.0,
+                margin: EdgeInsets.only(top: 60.0, right: 90.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF45E0EC),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 60.0,
+                width: 60.0,
+                margin: EdgeInsets.only(top: 70.0, right: 10.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFCE56),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Icon(
+                  Icons.directions_car,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 60.0,
+                width: 60.0,
+                margin: EdgeInsets.only(left: 70.0, top: 50.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFC6A7F),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Icon(
+                  Icons.place,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  bottom: 80.0,
+                ),
+                child: Text(
+                  'Quick Bee',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0,
+                    top: 10.0,
+                    right: 5.0,
+                  ),
+                  child: Container(
+                    height: 60.0,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF18D191),
+                        borderRadius: BorderRadius.circular(9.0)),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 5.0,
+                    top: 10.0,
+                    right: 20.0,
+                  ),
+                  child: Container(
+                    height: 60.0,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        color: Color(0xFF18D191),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 18.0,
+                  ),
+                  child: Text(
+                    'Create a new account',
+                    style: TextStyle(
+                      color: Color(0xFF18D191),
+                      fontSize: 17.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
